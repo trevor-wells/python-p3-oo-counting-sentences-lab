d746677504768e8b7a6bef3ad5c068366e2e59be
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-05-02 22:36:56 -0600

    confused

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    
+    def __init__(self, value = " "):
+        if type(value) == str:
+          self.value = value
+
+    def is_sentence(self):
+       return self.value[-1] == "."
+    
+    def is_question(self):
+       return self.value[-1] == "?"
+    
+    def is_exclamation(self):
+       return self.value[-1] == "!"
+    
+    def count_sentences(self):
+       new = self.value.replace("?",".")
+       new = new.replace("!",".")
+       new = new.split(".")
+       count = 0
+       for string in new:
+          print(string)
+          if string != "":
+             count += 1
+          else:
+             print("The value must be a string.")
+       return count
